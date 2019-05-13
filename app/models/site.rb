183c3c48dc3293b69e70748b8424f40aee3f8b57
Audric <audric.laroche@gmail.com>
2019-05-13 17:20:59 +0200

    add validates presence in model site

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,6 @@
 class Site < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :photo, presence: true
   mount_uploader :photo, PhotoUploader
 end
