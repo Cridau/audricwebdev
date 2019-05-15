fa6aef54c2b99919372dd8b49d6e6636477e4999
Audric <audric.laroche@gmail.com>
2019-05-15 16:40:30 +0200

    adding gem pundit and config authorized

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_13_100104) do
+ActiveRecord::Schema.define(version: 2019_05_14_130047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_05_13_100104) do
     t.datetime "updated_at", null: false
     t.text "articles"
     t.string "photo"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_sites_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,8 +34,10 @@ ActiveRecord::Schema.define(version: 2019_05_13_100104) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "sites", "users"
 end
